bdea0b70b0200e81aaad832aaf570319bde405db
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-27 08:50:08 +0200

    feat: add initial qoutes array with categories and placeholder functions for displaying and adding qoutes

---
+++ dom-manipulation/script.js
@@ -0,0 +1,26 @@
+const qoutes = [
+  {
+    text: "The journey doesn’t begin when you're ready—it begins when you start.",
+    category: "Motivation",
+  },
+  {
+    text: "Growth starts the moment you choose discipline over comfort.",
+    category: "Self-Improvement",
+  },
+  {
+    text: "You don’t have to be loud to be heard—clarity carries further than noise.",
+    category: "Wisdom",
+  },
+  {
+    text: "True creativity lies in showing the world what only you can see.",
+    category: "Creativity",
+  },
+  {
+    text: "Perfection is the enemy of momentum—choose progress every time.",
+    category: "Productivity",
+  },
+];
+
+const showRandomQuote = () => {};
+
+const createAddQuoteForm = () => {};
